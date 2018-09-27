bc442504c80896de0dfd8a674ca572d61944f90e
Smrithi Sunil <ssunil@bu.edu>
2018-09-27 13:49:35 -0400

    Add files via upload
    Updated to register LSCI and IOSI

--- getFuncSpeckleSpectral.m
+++ getFuncSpeckleSpectral.m
@@ -9,26 +9,24 @@
 % email: ssunil@bu.edu
 % BOAS Lab, Boston University
 
+% Updated 09/19/2018
+% Updated to register the LSCI and IOSI images
+% Updated to add CMRO2
+
 %% Load corresponding speckle and spectral files
 
-LSCI = load('D:\FlowVolume_04252018\04252018_Charlie_LSCI_2sec\LSCI');
-IOSI = load('D:\FlowVolume_04252018\04252018_Charlie_IOSI_2sec\IOSI');
+LSCI = load('D:\Stroke\CS10\Functional Activation\Baseline\Forepaw Stimulation\CS10_Baseline_LSCI_Forepaw\LSCI');
+IOSI = load('D:\Stroke\CS10\Functional Activation\Baseline\Forepaw Stimulation\CS10_Baseline_IOSI_Forepaw\IOSI');
 
-%% Convert contrast to flow for laser speckle data
+%% Average all the trials and calculate baseline 
 
+% LSCI average and response
 % assuming that flow is 1/K^2
 numTrials = input('Enter the number of trials in the session ');
 trialTime = input('Enter the length of each trial in seconds ');
-bin = 0.25;
-
+bin = 0.1;
 LSCI.time = LSCI.time - LSCI.time(1);
-figure
-subplot(3,2,[1 2])
-plot(LSCI.time',squeeze(mean(mean(LSCI.LSCI,1),2)),'LineWidth',2);
-xlabel('Time,s','FontSize',16)
-ylabel('Contrast','FontSize',16)
-title('Speckle contrast - full time series','FontSize',16)
-LSCI.time_LSC = 0:bin:trialTime-bin;
+LSCI.time_LSC = -5:bin:trialTime-bin-5;
 
 % trial average mean response 
 binLSCI = zeros(numTrials, size(LSCI.LSCI,1), size(LSCI.LSCI,2), trialTime/bin);
@@ -46,97 +44,14 @@ LSCI.LSCIavg = nanmean(binLSCI(:,:,:,:),1);
 LSCI.LSCIavg = permute(LSCI.LSCIavg, [2,3,4,1]);
 LSCI.flowIndex = 1./(LSCI.LSCIavg.*LSCI.LSCIavg);
 
-subplot(3,2,4)
-plot(LSCI.time_LSC',squeeze(mean(mean(LSCI.LSCIavg,1),2)),'LineWidth',3);
-xlabel('Time,s','FontSize',16)
-ylabel('Contrast','FontSize',16)
-title('Select baseline time')
-h = imrect;
-pos = wait(h);
-[~,bStart] = min(abs(LSCI.time_LSC-pos(1)));
-[~,bEnd] = min(abs(LSCI.time_LSC-pos(1)-pos(3)));
-if bStart < 1
-    bStart = 1;
-end
-if bEnd > size(LSCI.flowIndex,3)
-    bEnd = size(LSCI.flowIndex,3);
-end
-
-title('Select response time')
-h = imrect;
-pos = wait(h);
-[~,rStart] = min(abs(LSCI.time_LSC-pos(1)));
-[~,rEnd] = min(abs(LSCI.time_LSC-pos(1)-pos(3)));
-if rStart < 1
-    rStart = 1;
-end
-if rEnd > size(LSCI.flowIndex,3)
-    rEnd = size(LSCI.flowIndex,3);
-end
-title('Selected baseline and response times','FontSize',16)
-
+bStart = 1;
+bEnd = find(LSCI.time_LSC < 0,1,'last');
 LSCI.baseflowIndex = LSCI.flowIndex(:,:,bStart:bEnd); 
-LSCI.mBaseflowIndex = squeeze(mean(LSCI.baseflowIndex,3));
+LSCI.mBaseflowIndex = squeeze(nanmean(LSCI.baseflowIndex,3));
 
-LSCI.respflowIndex = LSCI.flowIndex(:,:,rStart:rEnd)./LSCI.mBaseflowIndex;
-LSCI.mRespflowIndex = squeeze(mean(LSCI.respflowIndex,3));
 
-% show flow map and select rectangular ROI
-subplot(3,2,3)
-imagesc(LSCI.mBaseflowIndex)
-caxis([prctile(LSCI.mBaseflowIndex(:),5),prctile(LSCI.mBaseflowIndex(:),95)]);
-title('Baseline flow','FontSize',16);
-colorbar
-axis image
-
-% show response map
-subplot(3,2,5)
-imagesc(LSCI.mRespflowIndex)
-caxis([prctile(LSCI.mRespflowIndex(:),5),prctile(LSCI.mRespflowIndex(:),95)]);
-title('Response map, select ROI');
-colorbar
-axis image
-
-%select and plot ROIs
-for i = 1:3
-    subplot(3,2,5)
-    h = imrect;
-    pos = wait(h);
-    delete(h);
-    hold on
-    plot([pos(1),pos(1),pos(1)+pos(3),pos(1)+pos(3),pos(1)],[pos(2),pos(2)+pos(4),pos(2)+pos(4),pos(2),pos(2)],'LineWidth',3);
-    hold off
-
-    pos = round(pos);
-    LSCI.flowIndexROI = LSCI.flowIndex(pos(2):1:pos(2)+pos(4),pos(1):1:pos(1)+pos(3),:);
-    LSCI.respROI = LSCI.flowIndexROI./LSCI.baseflowIndex(pos(2):1:pos(2)+pos(4),pos(1):1:pos(1)+pos(3));
-    LSCI.mRespRoi_LSC = squeeze(mean(mean(LSCI.respROI,1),2));
-
-    subplot(3,2,6)
-    hold on
-    plot(LSCI.time_LSC,LSCI.mRespRoi_LSC.*100,'LineWidth',3)
-    hold off
-    xlabel('Time, s','FontSize',16)
-    ylabel('Response, %','FontSize',16)
-    title('ROI normalized flow (trial averaged)','FontSize',16)
-    legend({'ROI 1','ROI 2','ROI 3'},'FontSize',12)
-end
-subplot(3,2,5)
-title('Response map','FontSize',16);
-
-%% Convert HbT into activation map for spectral data
-
-% IOSI post process
-% Average all trials in the session
-
-figure
-subplot(3,2,[1 2])
-IOSI.time_IOSfull = 0.1:0.1:IOSI.trialTime*20;
-plot(IOSI.time_IOSfull',squeeze(mean(mean(IOSI.HbT,1),2)),'LineWidth',2);
-xlabel('Time,s','FontSize',16)
-ylabel('HbT change','FontSize',16)
-title('HbT - full time series','FontSize',16)
-
+% IOSI average and response 
+IOSI.time_IOSfull = 0.1:0.1:IOSI.trialTime*IOSI.numTrials;
 IOSI.HbTreshape = zeros([IOSI.numTrials,size(IOSI.HbT,1),size(IOSI.HbT,2),IOSI.numFrame/IOSI.numTrials]);
 for t = 1:IOSI.numTrials
     IOSI.HbTreshape(t,:,:,:) = IOSI.HbT(:,:,(IOSI.numFrame/IOSI.numTrials*t)-(IOSI.numFrame/IOSI.numTrials-1):IOSI.numFrame/IOSI.numTrials*t);
@@ -145,82 +60,200 @@ IOSI.HbTavg = zeros([size(IOSI.HbT,1),size(IOSI.HbT,2),IOSI.numFrame/IOSI.numTri
 for i = 1:IOSI.numFrame/IOSI.numTrials 
     IOSI.HbTavg(:,:,i) = mean(squeeze(IOSI.HbTreshape(:,:,:,i)),1);  
 end
-
-IOSI.time_IOS = 0.1:0.1:IOSI.trialTime;
-
-subplot(3,2,4)
-plot(IOSI.time_IOS',squeeze(mean(mean(IOSI.HbTavg,1),2)),'LineWidth',3);
-xlabel('Time,s','FontSize',16)
-ylabel('HbT change','FontSize',16)
-title('Select baseline time')
-h = imrect;
-pos = wait(h);
-[~,bStart] = min(abs(IOSI.time_IOS-pos(1)));
-[~,bEnd] = min(abs(IOSI.time_IOS-pos(1)-pos(3)));
-if bStart < 1
-    bStart = 1;
-end
-if bEnd > size(IOSI.HbTavg,3)
-    bEnd = size(IOSI.HbTavg,3);
-end
-
-title('Select response time')
-h = imrect;
-pos = wait(h);
-[~,rStart] = min(abs(IOSI.time_IOS-pos(1)));
-[~,rEnd] = min(abs(IOSI.time_IOS-pos(1)-pos(3)));
-if rStart < 1
-    rStart = 1;
-end
-if rEnd > size(IOSI.HbTavg,3)
-    rEnd = size(IOSI.HbTavg,3);
-end
-title('Selected baseline and response times','FontSize',16)
-
+IOSI.time_IOS = -4.9:0.1:IOSI.trialTime-5;
+bStart = 1;
+bEnd = find(IOSI.time_IOS < 0,1,'last');
+IOSI.HbTavg = imrotate(IOSI.HbTavg,90);
 IOSI.basevolIndex = IOSI.HbTavg(:,:,bStart:bEnd); 
 IOSI.mBasevolIndex = squeeze(mean(IOSI.basevolIndex,3));
-IOSI.respvolIndex = IOSI.HbTavg(:,:,rStart:rEnd)-IOSI.mBasevolIndex;
+
+% Take the average of HbR
+IOSI.HbRreshape = zeros([IOSI.numTrials,size(IOSI.HbR,1),size(IOSI.HbR,2),IOSI.numFrame/IOSI.numTrials]);
+for t = 1:IOSI.numTrials
+    IOSI.HbRreshape(t,:,:,:) = IOSI.HbR(:,:,(IOSI.numFrame/IOSI.numTrials*t)-(IOSI.numFrame/IOSI.numTrials-1):IOSI.numFrame/IOSI.numTrials*t);
+end
+IOSI.HbRavg = zeros([size(IOSI.HbR,1),size(IOSI.HbR,2),IOSI.numFrame/IOSI.numTrials]);
+for i = 1:IOSI.numFrame/IOSI.numTrials 
+    IOSI.HbRavg(:,:,i) = mean(squeeze(IOSI.HbRreshape(:,:,:,i)),1);  
+end
+IOSI.HbRavg = imrotate(IOSI.HbRavg,90);
+
+%% Register LSCI and IOSI baseline images
+
+moving=double(LSCI.mBaseflowIndex);
+fixed=double(IOSI.mBasevolIndex);
+figure(99)
+subplot(1,2,1) 
+imagesc(moving)
+caxis([prctile(moving(:),5), prctile(moving(:),95)]); 
+subplot(1,2,2)
+imagesc(fixed)
+caxis([prctile(fixed(:),5), prctile(fixed(:),95)]); 
+colormap jet
+[x2, y2] = ginput(10);
+[D,Z,TRANSFORM] = procrustes([y2(2:2:10) x2(2:2:10)], [y2(1:2:10) x2(1:2:10)]);
+
+IOSI.regHbTavg = zeros(size(LSCI.flowIndex));
+IOSI.regHbRavg = zeros(size(LSCI.flowIndex));
+for z = 1:size(LSCI.flowIndex,3)
+    for u = 1:size(LSCI.mBaseflowIndex,1)
+        for v = 1:size(LSCI.mBaseflowIndex,2)
+            idx = round(TRANSFORM.b * [u v] * TRANSFORM.T + TRANSFORM.c(1,:));
+            if idx(1) < 1 || idx(1) > size(IOSI.HbTavg,1) || idx(2) < 1 || idx(2) > size(IOSI.HbTavg,2)
+                IOSI.regHbTavg(u,v,z) = 0;
+                IOSI.regHbRavg(u,v,z) = 0;
+            else
+                IOSI.regHbTavg(u,v,z) = IOSI.HbTavg(idx(1),idx(2),z);
+                IOSI.regHbRavg(u,v,z) = IOSI.HbRavg(idx(1),idx(2),z);
+            end
+        end
+    end
+end
+
+
+img = zeros(size(LSCI.mBaseflowIndex));
+for u = 1:size(LSCI.mBaseflowIndex,1)
+    for v = 1:size(LSCI.mBaseflowIndex,2)
+        idx = round(TRANSFORM.b * [u v] * TRANSFORM.T + TRANSFORM.c(1,:));
+        if idx(1) < 1 || idx(1) > size(IOSI.HbTavg,1) || idx(2) < 1 || idx(2) > size(IOSI.HbTavg,2)
+            img(u,v) = 0;
+        else
+            img(u,v) = IOSI.mBasevolIndex(idx(1),idx(2));
+        end
+    end
+end
+
+% crop images
+figure(100)
+subplot(1,2,1)
+imagesc(moving) 
+caxis([prctile(moving(:),5), prctile(moving(:),95)])
+subplot(1,2,2)
+imagesc(img)
+caxis([prctile(img(:),5), prctile(img(:),95)])
+colormap jet
+disp('Select crop region')
+h = imrect(gca, [20 20 150 150]);
+addNewPositionCallback(h,@(p) title(mat2str(p,3)));
+fcn = makeConstrainToRectFcn('imrect',get(gca,'XLim'),get(gca,'YLim'));
+setPositionConstraintFcn(h,fcn);
+position = wait(h);
+x(1) = int32(position(1));
+x(2) = x(1)+int32(position(3));
+y(1) = int32(position(2));
+y(2) = y(1)+int32(position(4));
+LSCI.flowIndex = LSCI.flowIndex(y(1):y(2),x(1):x(2),:);
+IOSI.regHbTavg = IOSI.regHbTavg(y(1):y(2),x(1):x(2),:);
+IOSI.regHbRavg = IOSI.regHbRavg(y(1):y(2),x(1):x(2),:);
+
+clear LSCI.mBaseflowIndex LSCI.baseflowIndex
+clear IOSI.mBasevolIndex IOSI.basevolIndex
+
+%% Plot baseline and response data
+
+bStart = 1;
+bEnd = find(LSCI.time_LSC < 0,1,'last');
+rStart = find(LSCI.time_LSC > 0,1,'first');
+rEnd = find(LSCI.time_LSC > 5,1,'first');
+LSCI.baseflowIndex = LSCI.flowIndex(:,:,bStart:bEnd); 
+LSCI.mBaseflowIndex = squeeze(nanmean(LSCI.baseflowIndex,3));
+LSCI.respflowIndex = LSCI.flowIndex(:,:,rStart:rEnd)./LSCI.mBaseflowIndex;
+LSCI.mRespflowIndex = squeeze(nanmean(LSCI.respflowIndex,3));
+
+bStart = 1;
+bEnd = find(IOSI.time_IOS < 0,1,'last');
+rStart = find(IOSI.time_IOS > 0,1,'first');
+rEnd = find(IOSI.time_IOS > 5,1,'first');
+IOSI.basevolIndex = IOSI.regHbTavg(:,:,bStart:bEnd); 
+IOSI.mBasevolIndex = squeeze(mean(IOSI.basevolIndex,3));
+IOSI.respvolIndex = (IOSI.regHbTavg(:,:,rStart:rEnd)+(100*10^-6))./((100*10^-6));
 IOSI.mRespvolIndex = squeeze(mean(IOSI.respvolIndex,3));
 
-% show vol map and select rectangular ROI
-subplot(3,2,3)
-imagesc(IOSI.mBasevolIndex)
-caxis([prctile(IOSI.mBasevolIndex(:),5),prctile(IOSI.mBasevolIndex(:),95)]);
-title('Baseline HbT','FontSize',16);
+% show flow map and select rectangular ROI
+figure(1);
+subplot(2,2,1)
+imagesc(LSCI.mBaseflowIndex)
+caxis([prctile(LSCI.mBaseflowIndex(:),5),prctile(LSCI.mBaseflowIndex(:),95)]);
+title('Baseline flow','FontSize',16);
+colormap jet
 colorbar
 axis image
-
 % show response map
-subplot(3,2,5)
-imagesc(IOSI.mRespvolIndex)
-caxis([prctile(IOSI.mRespvolIndex(:),5),prctile(IOSI.mRespvolIndex(:),95)]);
+figure(1)
+subplot(2,2,2)
+imagesc(LSCI.mRespflowIndex)
+caxis([prctile(LSCI.mRespflowIndex(:),5),prctile(LSCI.mRespflowIndex(:),95)]);
+% caxis([90 120])
 title('Response map, select ROI');
 colorbar
 axis image
 
-%select and plot ROIs
+% show vol map and select rectangular ROI
+figure(2)
+subplot(2,2,1)
+imagesc(IOSI.mBasevolIndex)
+caxis([prctile(IOSI.mBasevolIndex(:),5),prctile(IOSI.mBasevolIndex(:),95)]);
+title('Baseline HbT','FontSize',16);
+colormap jet
+colorbar
+axis image
+% show response map
+figure(2)
+subplot(2,2,2)
+imagesc(IOSI.mRespvolIndex)
+caxis([prctile(IOSI.mRespvolIndex(:),5),prctile(IOSI.mRespvolIndex(:),95)]);
+% caxis([-1 2.5])
+title('Response map, select ROI');
+colorbar
+axis image
+
+% select and plot ROIs
 for i = 1:3
-    subplot(3,2,5)
+    figure(1)
+    subplot(2,2,2)
     h = imrect;
     pos = wait(h);
+    pos = round(pos);
     delete(h);
     hold on
     plot([pos(1),pos(1),pos(1)+pos(3),pos(1)+pos(3),pos(1)],[pos(2),pos(2)+pos(4),pos(2)+pos(4),pos(2),pos(2)],'LineWidth',3);
     hold off
-
-    pos = round(pos);
-    IOSI.volIndexROI = IOSI.HbTavg(pos(2):1:pos(2)+pos(4),pos(1):1:pos(1)+pos(3),:);
-    IOSI.respROI = IOSI.volIndexROI-IOSI.basevolIndex(pos(2):1:pos(2)+pos(4),pos(1):1:pos(1)+pos(3));
-    IOSI.mRespRoi_IOS = squeeze(mean(mean(IOSI.respROI,1),2));
-
-    subplot(3,2,6)
+    LSCI.flowIndexROI = LSCI.flowIndex(pos(2):1:pos(2)+pos(4),pos(1):1:pos(1)+pos(3),:);
+    LSCI.respROI = LSCI.flowIndexROI./LSCI.baseflowIndex(pos(2):1:pos(2)+pos(4),pos(1):1:pos(1)+pos(3));
+    LSCI.mRespRoi_LSC = squeeze(mean(mean(LSCI.respROI,1),2));
+    figure(1)
+    subplot(2,2,[3 4])
     hold on
-    plot(IOSI.time_IOS',IOSI.mRespRoi_IOS,'LineWidth',3)
+    plot(LSCI.time_LSC,LSCI.mRespRoi_LSC,'LineWidth',3)
     hold off
     xlabel('Time, s','FontSize',16)
-    ylabel('Response','FontSize',16)
-    title('ROI HbT change (trial averaged)','FontSize',16)
+    ylabel('Fractional change','FontSize',16)
+    title('ROI flow','FontSize',16)
+    legend({'ROI 1','ROI 2','ROI 3'},'FontSize',12)
+    
+    figure(2)
+    subplot(2,2,2)
+    hold on
+    plot([pos(1),pos(1),pos(1)+pos(3),pos(1)+pos(3),pos(1)],[pos(2),pos(2)+pos(4),pos(2)+pos(4),pos(2),pos(2)],'LineWidth',3);
+    hold off
+    IOSI.volIndexROI = IOSI.regHbTavg(pos(2):1:pos(2)+pos(4),pos(1):1:pos(1)+pos(3),:);
+    IOSI.respROI = (IOSI.volIndexROI+(100*10^-6))./((100*10^-6));
+    IOSI.mRespRoi_IOS(i).roi = squeeze(mean(mean(IOSI.respROI,1),2));
+    figure(2)
+    subplot(2,2,[3 4])
+    hold on
+    plot(IOSI.time_IOS',IOSI.mRespRoi_IOS(i).roi,'LineWidth',3)
+    hold off
+    xlabel('Time, s','FontSize',16)
+    ylabel('Fractional change','FontSize',16)
+    title('ROI HbT','FontSize',16)
     legend({'ROI 1','ROI 2','ROI 3'},'FontSize',12)
 end
-subplot(3,2,5)
+figure(1)
+subplot(2,2,2)
 title('Response map','FontSize',16);
+figure(2)
+subplot(2,2,2)
+title('Response map','FontSize',16);
+
+
